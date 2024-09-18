5110f6ecf0963fb89211fc42d139e054edaef02c
ayushsem12 <ayushsem2002@gmail.com>
2024-09-19 03:44:22 +0530

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,9 +1,11 @@
+#The main method takes length and width as arguments to initialize an instance of the Rectangle class.
+
 class Rectangle:
     def main(self, length: int, width: int):
         self.length = length
         self.width = width
     
-  
+ #The __iter__ method allows the object to be iterable
     def __iter__(self):
         yield {'length': self.length}
         yield {'width': self.width}
